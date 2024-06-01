85f441b02dc3874302d618d8505808fe8b3b4672
Bernal Thiago <thiagoa.bernal@yahoo.com.ar>
2024-06-01 13:20:27 -0300

    Arreglo de sintaxis en structs.h

--- structs.h
+++ structs.h
@@ -4,55 +4,55 @@
 
 typedef struct fech{
 	int dia, mes, anio;
-};
+}fech;
 
 typedef struct tiempo{
 	int hora, minuto;
-};
+}tiempo;
 
 //LES
 typedef struct opciones{
 	int id;
 	char nombre[50];
 	float costoBase;
-	opciones *sgte;
+	struct opciones *sgte;
 }opciones;
 
 //LDE
 typedef struct tareas{
 	int id, idOpcion,orden;
 	tiempo duracion;
-	tareas *ant,*sgte;
+	struct tareas *ant,*sgte;
 }tareas;
 
 //ARBOL DE BUSQUEDA BINARIA
 typedef struct stock{
 	int id, stock;
-	char denominacion[50]; unidad[10];
+	char denominacion[50], unidad[10];
 	float precio;
-	stock *izq,*der;
+	struct stock *izq, *der;
 }stock;
 
 //LES
 typedef struct trabajos{
 	int id, idOpcion, altura, idTecnico, idCliente, finalizado;
 	char ubicacion[50];
-	fech fechaFin;
+	struct fech fechaFin;
 	float costoTotal; //la suma de la mano de obra(20% si es > 4 mts), materiales y costo base
-	trabajos *sgte;
+	struct trabajos *sgte;
 }trabajos;
 
 //PILA
 typedef struct clientes{
 	int id, dni;
 	char nombre[50];
-	clientes *sgte;
+	struct clientes *sgte;
 }clientes;
 
 //COLA
 typedef struct tecnicos{
 	int id, dni;
 	char nombre[50];
-	tecnicos *sgte;
+	struct tecnicos *sgte;
 }tecnicos;
 
