20aa876afeefc8071fd5f0fc6b50b7ec6fbd7d6e
Bernal Thiago <thiagoa.bernal@yahoo.com.ar>
2024-06-01 11:55:05 -0300

    arreglo de estructuras ya gregado de comentarios

--- structs.h
+++ structs.h
@@ -10,20 +10,22 @@ typedef struct tiempo{
 	int hora, minuto;
 };
 
+//LES
 typedef struct opciones{
 	int id;
 	char nombre[50];
 	float costoBase;
-	tiempo instalacion;
 	opciones *sgte;
 }opciones;
 
+//LDE
 typedef struct tareas{
 	int id, idOpcion,orden;
 	tiempo duracion;
 	tareas *ant,*sgte;
 }tareas;
 
+//ARBOL DE BUSQUEDA BINARIA
 typedef struct stock{
 	int id, stock;
 	char denominacion[50]; unidad[10];
@@ -31,33 +33,26 @@ typedef struct stock{
 	stock *izq,*der;
 }stock;
 
+//LES
 typedef struct trabajos{
 	int id, idOpcion, altura, idTecnico, idCliente, finalizado;
 	char ubicacion[50];
 	fech fechaFin;
-	float costoTotal;
+	float costoTotal; //la suma de la mano de obra(20% si es > 4 mts), materiales y costo base
 	trabajos *sgte;
 }trabajos;
 
+//PILA
 typedef struct clientes{
 	int id, dni;
 	char nombre[50];
 	clientes *sgte;
 }clientes;
 
+//COLA
 typedef struct tecnicos{
 	int id, dni;
 	char nombre[50];
 	tecnicos *sgte;
 }tecnicos;
 
-typedef struct trabajosPendientes{
-	int idTrabajos;
-	trabajosPendientes *sgte;
-}trabajosPendientes;
-
-
-typedef struct tareasPendientes{
-	int idTrabajosPendientes, idTareas;
-	tareasPendientes *sgte;
-}tareasPendientes;
